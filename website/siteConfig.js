4ab2e09edbefd79dd9ba6b5d8a6e0386e2c62595
raglima <rafael@3wrsolucoes.com.br>
2025-09-15 22:45:40 -0300

    Commit 6

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'GD Control', // Title for your website.
   tagline: 'Documentação',
-  url: 'https://doc.gdcontrol.com.br', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://gdtechdoc.pages.dev', // Your website URL
+  baseUrl: '/gdtechdoc/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
